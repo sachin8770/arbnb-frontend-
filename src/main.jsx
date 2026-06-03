3e0820ebb3578eb738f2d419ec544f0db6323c7d
sachin 8770 <sachinpatel.0950@gmail.com>
2026-06-03 22:59:44 +0530

    your message here

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./store/store";
+import store from "./store/store.js";
 import "./index.css";
 
 import App from "./App.jsx";
